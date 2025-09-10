f4d0ecdef8fcb0d2967124e176562341776e0038
John McCambridge <john@lumenary.com>
2025-09-09 17:58:30 -0700

    updated template

---
+++ eslint.config.mjs
@@ -0,0 +1,69 @@
+import eslintReact from '@eslint-react/eslint-plugin';
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  globalIgnores([
+    'dist',
+    'node_modules',
+    'scripts/*',
+    'config/*',
+    'pnpm-lock.yaml',
+    'pnpm-workspace.yaml',
+    '.DS_Store',
+    'package.json',
+    'tsconfig.json',
+    '**/*.md',
+    'build',
+    '.eslintrc.cjs',
+    'eslint.config.js',
+  ]),
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      eslintReact.configs['recommended-typescript'],
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        // Enable project service for better TypeScript integration
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+      ecmaVersion: 2022,
+      globals: globals.browser,
+      sourceType: 'module',
+    },
+    rules: {
+      '@eslint-react/no-leaked-conditional-rendering': 'warn',
+      '@stylistic/jsx-curly-brace-presence': [
+        'warn',
+        { props: 'never', children: 'never', propElementValues: 'always' },
+      ],
+      '@stylistic/quotes': ['warn', 'single'],
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'separate-type-imports',
+        },
+      ],
+    },
+  },
+]);
